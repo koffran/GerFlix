7e9f893b7aedb54fc6a714a8afda7336273d2394
Franco Zarate <fran.e.zarate@gmail.com>
2018-09-28 09:57:50 -0300

    Add files via upload

---
+++ GerFlix-master/GerFlix-master/GerFlix 1.0/Usuario.c
@@ -0,0 +1,85 @@
+
+#include "Usuario.h"
+
+void inicializarUsuariosEstado(eUsuario usuarios[], int cant)
+{
+    int i;
+
+    for(i=0; i<cant; i++)
+    {
+        usuarios[i].estado = 0;
+    }
+}
+void inicializarUsuariosHardCode(eUsuario usuarios[])
+{
+    int id[15] = {1000,1001,1002,1003,1004, 1005,1006,1007,1008,1009, 1010,1011,1012,1013,1014};
+    char nombre[][50]= {"Juan","Maria","Pedro","Vanesa","Jose","Luisa","Laura","Marcelo","German","Victoria","Dafne","Antonela","Gisele","Dario","Pedro"};
+
+    int serie[15] = {100,100,101,101,102,100,100,103,101,102,103,101,100,100,101};
+    int i;
+
+    for(i=0; i<15; i++)
+    {
+        usuarios[i].idUsuario=id[i];
+        usuarios[i].idSerie=serie[i];
+        usuarios[i].estado = 1;
+        strcpy(usuarios[i].nombre, nombre[i]);
+    }
+}
+
+void mostrarUsuario (eUsuario usuario)
+{
+    if(usuario.estado == 1)
+    {
+         printf("%s\n",usuario.nombre);
+    }
+
+}
+
+void mostrarListaUsuarios(eUsuario* lista, int tam)
+{
+    int i;
+    for (i=0;i<tam;i++)
+    {
+        mostrarUsuario(lista[i]);
+    }
+}
+
+
+void mostrarUsuarioConSuSerie(eUsuario* usuarios , int tamUsu, eSerie* series, int tamSer)
+{
+    int i,j;
+    for (i=0; i<tamUsu; i++)
+    {
+        if (usuarios[i].estado == 1  )
+        {
+            printf ("\n%s: \n",usuarios[i].nombre);
+            for (j=0; j<tamSer;j++)
+            {
+                if(usuarios[i].idSerie == series[j].idSerie)
+                {
+                    printf("%8s\n",series[j].nombre);
+                }
+            }
+        }
+    }
+}
+
+void mostrarSerieConUsuarios (eSerie* series, int tamSer,eUsuario* usuarios, int tamUsu)
+{
+    int i,j;
+    for (i=0; i<tamSer; i++)
+    {
+        if (series[i].estado == 1)
+        {
+            printf ("\n%s: \n",series[i].nombre);
+            for (j=0; j<tamUsu;j++)
+            {
+                if(series[i].idSerie == usuarios[j].idSerie)
+                {
+                    printf("%8s\n",usuarios[j].nombre);
+                    }
+            }
+        }
+    }
+}
---
+++ GerFlix-master/GerFlix-master/GerFlix 1.0/main.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Usuario.h"
+#define TAMSERIE 20
+#define TAMUSUARIO 100
+
+int main()
+{
+    eSerie listaDeSeries[TAMSERIE];
+    eUsuario listaDeUsuarios[TAMUSUARIO];
+
+    inicializarSeriesEstado(listaDeSeries, TAMSERIE);
+    inicializarSeriesHardCode(listaDeSeries);
+
+    inicializarUsuariosEstado(listaDeUsuarios,TAMUSUARIO);
+    inicializarUsuariosHardCode(listaDeUsuarios);
+
+    char rta = 's';
+    int opcion;
+
+    while(rta=='s')
+    {
+        /*  1. Mostrar el listado de series
+            2. Mostrar el listado de usuarios
+            3. Mostrar el listado de Usuarios con el nombre de la serie que ve
+            4. Mostrar por cada serie, el nombre de los usuarios que la ven.
+        */
+        system("cls");
+        printf("1- Mostrar series\n");
+        printf("2- Mostrar usuarios\n");
+        printf("3- Mostrar el listado de usuarios con el nombre de la serie que ve\n");
+        printf("4- Mostrar por cada serie el nombre de los usuarios que la ven\n\n");
+        printf("5- Salir\n");
+        scanf("%d",&opcion);
+
+        switch(opcion)
+        {
+            case 1:
+                system("cls");
+                mostrarListaSeries(listaDeSeries,TAMSERIE);
+                system("pause");
+                break;
+            case 2:
+                system("cls");
+                mostrarListaUsuarios(listaDeUsuarios,TAMUSUARIO);
+                system("pause");
+                break;
+            case 3:
+                system("cls");
+                mostrarUsuarioConSuSerie(listaDeUsuarios,TAMSERIE,listaDeSeries,TAMSERIE);
+                system("pause");
+                break;
+            case 4:
+                system("cls");
+                mostrarSerieConUsuarios (listaDeSeries,TAMSERIE,listaDeUsuarios,TAMUSUARIO);
+                system("pause");
+                break;
+            case 5:
+                rta = 'n';
+                break;
+            default:
+                printf("Ingrese una opcion valida <1-5>\n");
+                system("Pause");
+        }
+    }
+    return 0;
+}
